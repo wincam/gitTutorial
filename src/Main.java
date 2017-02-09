6e865564414cd049373175acdb9aeebac496f311
Cameron Nicolle <cameron.nicolle@outlook.com>
2017-02-09 13:23:10 -0500

    Completed feature 2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
  */
 public class Main {
     public static void main(String[] args){
-        Person p1 = new Student("Bob", "bob@uwo.ca", "Restaurant Management");
+        Person p1 = new Student("Bob", "bob@uwo.ca", "Restaurant Management", true);
         Person p2 = new Instructor("Alice", "aline@uwo.ca", "Assistant Professor");
 
         System.out.println(p1);
--- src/Student.java
+++ src/Student.java
@@ -3,10 +3,12 @@
  */
 public class Student extends Person {
     String major;
+    boolean fulltime;
 
-    public Student(String name, String emailAddress, String major) {
+    public Student(String name, String emailAddress, String major, boolean fulltime) {
         super(name, emailAddress);
         this.major = major;
+        this.fulltime = fulltime;
     }
 
     public String toString() {
